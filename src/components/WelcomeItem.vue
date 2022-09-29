5c4f0c1c18893108e4cfe60fdeefc206e50f0c7f
zhouhuafei <1123486116@qq.com>
2022-09-29 11:53:39 +0800

    feat: 🎸 about

--- src/components/WelcomeItem.vue
+++ src/components/WelcomeItem.vue
@@ -43,12 +43,12 @@ h3 {
 @media (min-width: 1024px) {
   .item {
     margin-top: 0;
-    padding: 0.4rem 0 1rem calc(var(--section-gap) / 2);
+    padding: 0.4rem 0 1rem 5rem;
   }
 
   i {
     top: calc(50% - 25px);
-    left: -26px;
+    left: 1rem;
     position: absolute;
     border: 1px solid var(--color-border);
     background: var(--color-background);
